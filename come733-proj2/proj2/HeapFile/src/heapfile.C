bda197a0bbae2a333e114c184944c7260b0f7f76
Seoharin <72519646+Seoharin@users.noreply.github.com>
2022-04-26 00:17:37 +0900

    Update heapfile.C

--- come733-proj2/proj2/HeapFile/src/heapfile.C
+++ come733-proj2/proj2/HeapFile/src/heapfile.C
@@ -533,6 +533,7 @@ Status allocateDirSpace(struct DataPageInfo * dpinfop,
     int pageId;
     Page *pg;
     int i;
+    
   
     
     //check existing pages
@@ -545,14 +546,14 @@ Status allocateDirSpace(struct DataPageInfo * dpinfop,
             pg = (Page *)page;
           //  int num = MINIBASE_BM->getNumUnpinnedBuffers();
             allocDirPageId = page->page_no();
-            Status pinStatus = MINIBASE_BM->pinPage(allocDirPageId,pg,page->empty(),FileName);
+            Status pinStatus = MINIBASE_BM->pinPage(allocDirPageId,pg,page->empty(),&FileName);
             if(pinStatus!=OK)
                 return MINIBASE_CHAIN_ERROR(BUFMGR,pinStatus);
 
             Status status = page->insertRecord((char *)dpinfop,sizeof(DataPageInfo),allocDataPageRid);
             if(status!=OK)
                 return MINIBASE_CHAIN_ERROR(HEAPFILE,status);
-            Status unpinStatus = MINIBASE_BM->unpinPage(allocDirPageId,DIRTY,FileName);
+            Status unpinStatus = MINIBASE_BM->unpinPage(allocDirPageId,DIRTY,&FileName);
             if(unpinStatus!=OK)
                 return MINIBASE_CHAIN_ERROR(BUFMGR,unpinStatus);
           //  num = MINIBASE_BM->getNumUnpinnedBuffers();
@@ -577,13 +578,13 @@ Status allocateDirSpace(struct DataPageInfo * dpinfop,
     Status status = dirPage->insertRecord((char *)dpinfop,sizeof(DataPageInfo),allocDataPageRid);
     if(status!=OK)
         return MINIBASE_CHAIN_ERROR(HEAPFILE,status);
-    status = MINIBASE_BM->unpinPage(allocDirPageId,DIRTY,FileName);
+    status = MINIBASE_BM->unpinPage(allocDirPageId,DIRTY,&FileName);
     if(status!=OK)
         return MINIBASE_CHAIN_ERROR(BUFMGR,status);
     directoryPages.push_back(dirPage);
     if(directoryPages.size()==1)
     {
-        Status status = MINIBASE_DB->add_file_entry(FileName,dirPage->page_no());
+        Status status = MINIBASE_DB->add_file_entry(&FileName,dirPage->page_no());
         directory d;
         d.pages = directoryPages;
         d.headerPageId = dirPage->page_no();
