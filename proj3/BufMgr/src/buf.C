ce7f5b5932fa4a1984cbcc6a2211bf0a870020ac
Seoharin <72519646+Seoharin@users.noreply.github.com>
2022-05-25 20:46:22 +0900

    Update buf.C

--- proj3/BufMgr/src/buf.C
+++ proj3/BufMgr/src/buf.C
@@ -193,14 +193,14 @@ Status BufMgr::newPage(PageId& firstPageId, Page*& firstpage, int howmany) {
   for(int i=0;i<this->numBuffers;i++){
     if(bufDescr[i].page_number==INVALID_PAGE){
        MINIBASE_DB->allocate_page(firstPageId,howmany);
-
-       write_hash_table(firstPageId)
+       int frame = FindFrame(firstPageId);
+       write_hash_table(firstPageId,frame);
        
        bufDescr[i].page_number = firstPageId;
        bufDescr[i].pin_count=1;
        bufDescr[i].dirty=false;
 
-       int frame = FindFrame(firstPageId);
+       
        memmove(bufPool+frame, firstpage, sizeof(Page));
     }
   }
@@ -362,8 +362,10 @@ void BufMgr::delete_hash_table(PageId page_number){
 
 PageId BufMgr::Find_Replacement_Page(){
   PageId pageid;
-  if(MRU.size()>0){
-    int msize = MRU.size();
+  int msize = MRU.size();
+  int lsize = LRU.size();
+  if(msize>0){
+    
     pageid = MRU.back();
     MRU.resize(msize-1);
     if(pageid==LRU.front()){
@@ -374,8 +376,8 @@ PageId BufMgr::Find_Replacement_Page(){
     }
     return pageid;
 
-  }else if(LRU.size()>0){
-    int lsize = LRU.size();
+  }else if(lsize()>0){
+   
     pageid = LRU.front();
     for(int i=0;i<lsize-1;i++){
       LRU[i]=LRU[i+1];
